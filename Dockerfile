c89b5efea6ef7c2240ac966497987fbe0ec62c3d
Jeremie <jeremie.chaloin@gmail.com>
2021-07-03 23:27:37 +0200

    apt configure crash build

--- Dockerfile
+++ Dockerfile
@@ -11,19 +11,11 @@ RUN apt-get update && apt-get upgrade -y && \
   apt-get install -y --no-install-recommends \
   # S6 Install
   ca-certificates wget \
-  dirmngr gnupg \
   binutils net-tools bc expect procps && \
   # Chmod scripts
   chmod +x /scripts/*.sh && \
   # i386 & amd64 specific
   /scripts/get-qemu.sh && \
-  # Import GPG key for the APT repository
-  KEY_ID=C969F07840C430F5 && \
-  apt-key adv --recv-key --keyserver hkp://keyserver.ubuntu.com:80 ${KEY_ID} || \
-  apt-key adv --recv-key --keyserver hkp://pool.sks-keyservers.net:80 ${KEY_ID} || \
-  apt-key adv --recv-key --keyserver hkp://pgp.mit.edu:80 ${KEY_ID} && \
-  # FlightRadar Repository
-  echo "deb http://repo.feed.flightradar24.com flightradar24 raspberrypi-stable" > /etc/apt/sources.list.d/fr24.list && \
   # Arch Specific Operations
   /scripts/arch-specific.sh && \
   # S6 OVERLAY
@@ -31,7 +23,7 @@ RUN apt-get update && apt-get upgrade -y && \
   # Healthcheck
   chmod +x /healthcheck.sh && \
   # Cleanup
-  apt-get remove -y ca-certificates wget gnupg && \
+  apt-get remove -y ca-certificates wget && \
   apt-get autoremove -y && \
   rm -rf /scripts /tmp/* /var/lib/apt/lists/*
 
--- rootfs/scripts/arch-specific.sh
+++ rootfs/scripts/arch-specific.sh
@@ -1,44 +1,43 @@
 #!/bin/sh
-arch=$(dpkg --print-architecture)
 
+FR24FILE="https://repo-feed.flightradar24.com/rpi_binaries/fr24feed_1.0.28-1_armhf.deb"
+if wget -O /tmp/fr24feed.deb $FR24FILE; then
+  echo "Download OK"
+else
+  echo "Download failed!"
+  exit 1
+fi
+
+# Deploy fr24feed.deb
+cd /tmp || exit 1
+ar xv fr24feed.deb
+tar xzvf data.tar.gz
+mv -v /tmp/usr/bin/* /usr/bin/
+mv -v /tmp/usr/share/fr24 /usr/share/
+
+mkdir -p /var/log/fr24feed || true
+chmod -R a+rx /var/log/fr24feed || true
+mkdir -p /run/fr24feed || true
+chmod -R a+rx /run/fr24feed || true
+
+arch=$(dpkg --print-architecture)
 case $arch in
   amd64)
-    dpkg --add-architecture armhf
-    apt-get update
-    apt-get install --no-install-recommends \
-    -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" \
-    -y fr24feed:armhf
     rm -f /etc/services.d/fr24feed/run
     mv /etc/services.d/fr24feed/run.qemu /etc/services.d/fr24feed/run    
     ;;
   i386)
-    dpkg --add-architecture armhf
-    apt-get update
-    apt-get install --no-install-recommends \
-    -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" \
-    -y fr24feed:armhf
     rm -f /etc/services.d/fr24feed/run
     mv /etc/services.d/fr24feed/run.qemu /etc/services.d/fr24feed/run    
     ;;
   armhf)
-    apt-get update
-    apt-get install --no-install-recommends \
-    -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" \
-    -y fr24feed
+    rm -f /etc/services.d/fr24feed/run.qemu
     ;;
   arm64)
-    dpkg --add-architecture armhf
-    apt-get update
-    apt-get install --no-install-recommends \
-    -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" \
-    -y fr24feed:armhf
+    rm -f /etc/services.d/fr24feed/run.qemu
     ;;
   armel)
-    dpkg --add-architecture armhf
-    apt-get update
-    apt-get install --no-install-recommends \
-    -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" \
-    -y fr24feed:armhf
+    rm -f /etc/services.d/fr24feed/run.qemu
     ;;
   *)
     exit 1
