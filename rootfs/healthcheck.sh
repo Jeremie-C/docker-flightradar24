a2a2803ba9d72640a4f9d151f977f8d39b17c367
Jeremie <jeremie.chaloin@gmail.com>
2021-07-09 23:13:33 +0200

    Correct ShellCheck error

--- rootfs/healthcheck.sh
+++ rootfs/healthcheck.sh
@@ -3,7 +3,7 @@ set -e
 EXITCODE=0
 
 # FR24 Feeder
-SRV_DEATHS=$(s6-svdt /run/s6/services/fr24feed | grep -cv "exitcode 0" | wc -l)
+SRV_DEATHS=$(s6-svdt /run/s6/services/fr24feed | grep -cv "exitcode 0")
 if [ "$SRV_DEATHS" -ge 1 ]; then
     echo "readsb deaths: $SRV_DEATHS. UNHEALTHY"
     EXITCODE=1
@@ -13,7 +13,7 @@ fi
 s6-svdt-clear /run/s6/services/fr24feed
 
 # Redirect logs
-SRV_DEATHS=$(s6-svdt /run/s6/services/fr24logs | grep -cv "exitcode 0" | wc -l)
+SRV_DEATHS=$(s6-svdt /run/s6/services/fr24logs | grep -cv "exitcode 0")
 if [ "$SRV_DEATHS" -ge 1 ]; then
     echo "readsb deaths: $SRV_DEATHS. UNHEALTHY"
     EXITCODE=1
